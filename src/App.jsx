3823584d798776f853c7671dfd89a34b7e518ebb
Shamim Ahammad <shamimfstack@gmail.com>
2023-12-20 08:35:39 +0600

    image upload functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ image, setImage ] = useState("")
+
+  const submitImage = () => {
+    const data = new FormData();
+    data.append("file", image);
+    // data.append("upload_preset", "upload_preset_name")
+    data.append("upload_preset", "shamimfstack")
+    // data.append("cloud_name", "cloud_name")
+    data.append("cloud_name", "dixhlm8rp")
+
+    fetch("https://api.cloudinary.com/v1_1/dixhlm8rp/image/upload", {
+      method: "POST",
+      body: data
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
 
   return (
     <>
-      <h2>App</h2>
+      <div>
+        <div>
+          <input type="file" onChange={e => setImage(e.target.files[0])} />
+          <button onClick={submitImage}>upload</button>
+        </div>
+        { image ? <img src={image} /> : "No image chosen"}
+      </div>
     </>
   )
 }
 
 export default App
+ 
